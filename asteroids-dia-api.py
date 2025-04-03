3b2b8ae471ca010361d531f2dd78d3b94a7f532c
NYT03 <nikhilraval706@gmail.com>
2025-04-03 14:51:18 +0530

    ip-changed

--- asteroids-dia-api.py
+++ asteroids-dia-api.py
@@ -67,4 +67,5 @@ def predict():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Bind to all available interfaces and specify port 5000
+    app.run(host='0.0.0.0', port=5000, debug=True)
